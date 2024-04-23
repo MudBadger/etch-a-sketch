b69f3c1cad468d77e74441a47585c0e7ad95ba01
Gaëtan Charolle <gaetan.charolle@gmail.com>
2024-04-23 21:25:03 +0200

    ajout fonction pour créer sketch

---
+++ app.js
@@ -0,0 +1,50 @@
+const gridContainer = document.querySelector("#grid-container");
+
+function grid() {
+    !squareNumber && (squareNumber = 16);
+    for (let i = 0; i < squareNumber; i++) {
+        let row = document.createElement("div");
+        gridContainer.appendChild(row);
+        row.className = "row";
+        for (let i = 0; i < squareNumber; i++) {
+            let square = document.createElement("div");
+            row.appendChild(square);
+            square.className = "square";
+            square.addEventListener("mouseover", (event) => {
+                let rgbArray = [];
+                for (let i = 0; i < 3; i++) {
+                    let color = Math.floor(Math.random() * 255);
+                    console.log(color);
+                    rgbArray.push(color);
+                }
+                console.log(rgbArray.toString());
+                event.target.style = `background-color:rgb(${rgbArray.toString()})`;
+            });
+        }
+    }
+}
+
+function clearGrid() {
+    gridContainer.innerHTML = "";
+    grid();
+}
+
+let squareNumber;
+
+const sizeBtn = document.querySelector("#size");
+
+sizeBtn.addEventListener("click", (event) => {
+    squareNumber = prompt("Enter a grid size (max 100) : ");
+    if (squareNumber <= 100) {
+        clearGrid();
+    } else {
+        alert("Please enter a maximum grid size of 100");
+    }
+});
+
+const clearBtn = document.querySelector("#clear");
+clearBtn.addEventListener("click", (event) => {
+    clearGrid();
+});
+
+grid();
